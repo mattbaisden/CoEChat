58c13916a92724c4800259658a474755c0d117a9
mattbaisden <matthewray.baisden@calbaptist.edu>
2022-10-30 22:19:57 -0700

    edits

--- server/index.js
+++ server/index.js
@@ -3,25 +3,23 @@ const app = express();
 const http = require("http");
 const cors = require("cors");
 const { Server } = require("socket.io");
-
 app.use(cors());
 
-const server = require("http").createServer(app);
+const server = http.createServer(app);
 
 const io = new Server(server, {
-  /*cors: {
-    origin: "*",
+  cors: {
+    origin: "http://localhost:3000",
     methods: ["GET", "POST"],
-    credentials:true
-  }, */
+  },
 });
 
 io.on("connection", (socket) => {
-  console.log(`User Connected: ${socket.id}`);
+  console.log('User Connected: ${socket.id}');
 
   socket.on("join_room", (data) => {
     socket.join(data);
-    console.log(`User with ID: ${socket.id} joined room: ${data}`);
+    console.log('User with ID: ${socket.id} joined room: ${data}');
   });
 
   socket.on("send_message", (data) => {
